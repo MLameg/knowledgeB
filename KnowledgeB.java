189f4363be60934f75bc7b6fbc88e3c5654e3d41
MLameg <MLameg12@gmail.com>
2018-02-22 20:30:26 -0500

    Add files via upload

---
+++ KnowledgeB.java
@@ -0,0 +1,28 @@
+//***************************************************************************
+//  Knowledge is Power: Page 55  PP 1.8 
+//  By TJ Lameg
+//*************************************************************************** 
+
+public class KnowledgeB 
+{
+    //***********************************************************************
+	//Part A:  One Line
+	//***********************************************************************
+	public static void main(String [] args){
+    System.out.println("Knowledge is Power");
+	System.out.println("");
+	//***********************************************************************
+	//Part B:  On 3 Lines, One Word Per Line, Centered
+	//***********************************************************************
+	
+    System.out.println("Knowledge");
+    System.out.println("    is   ");
+    System.out.println("  Power  ");
+
+    //***********************************************************************
+    //Pact C:  Inside a Box Made up of = and |
+    //***********************************************************************
+    System.out.println("");
+    System.out.println("==========================");
+	System.out.println("||   Knowledge is Power ||");
+	System.out.println("==========================");}}
